1bd11c703948b3b38a69d1ffce9e30f3f777865e
Marc0-Leal <marc.leals@duocuc.cl>
2026-04-17 16:09:02 -0400

    first commit

---
+++ src/main/java/com/example/actividad/spring_boot_desing_patters/SpringBootDesingPattersApplication.java
@@ -0,0 +1,13 @@
+package com.example.actividad.spring_boot_desing_patters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDesingPattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDesingPattersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actividad/spring_boot_desing_patters/SpringBootDesingPattersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actividad.spring_boot_desing_patters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDesingPattersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
